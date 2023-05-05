43255fb3bd3bb8703b7ad7d9327ebf201502e9a0
lukasimonishvili <37000830+lukasimonishvili@users.noreply.github.com>
2023-05-06 00:43:57 +0400

    print full name

---
+++ Homework 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Luka Simonishvili");
+        }
+    }
+}
