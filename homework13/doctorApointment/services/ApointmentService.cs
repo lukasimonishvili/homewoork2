7671975d9ef990b8c2df325c24982e90b8e823f5
lukasimonishvili <37000830+lukasimonishvili@users.noreply.github.com>
2023-06-07 05:53:59 +0400

    validation

--- homework13/doctorApointment/services/ApointmentService.cs
+++ homework13/doctorApointment/services/ApointmentService.cs
@@ -1,11 +1,9 @@
 ﻿using doctorApointment.Models;
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-
 
 namespace doctorApointment.services
 {
@@ -14,11 +12,27 @@ namespace doctorApointment.services
         private static string filesDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location).Split("bin")[0] + "/data";
         private static string JSONFileDirectory = filesDirectory + "/data.json";
 
+        private static bool IsValidTime(string timeString)
+        {
+            string[] timeParts = timeString.Split(':');
+            if (timeParts.Length != 2)
+                return false;
+
+            if (!int.TryParse(timeParts[0], out int hours) || !int.TryParse(timeParts[1], out int minutes))
+                return false;
+
+            if (hours == 19 && minutes != 0)
+                return false;
+            if (hours >= 10 && hours <= 19 && minutes >= 0 && minutes <= 59)
+                return true;
+
+            return false;
+        }
+
         public static bool saveNewApointment(Apointment data)
         {
-            var hours = Convert.ToInt32(data.Time.Split(":")[0]);
-
-            if (hours < 10 || hours > 19)
+            var isTimeValid = IsValidTime(data.Time);
+            if (!isTimeValid)
             {
                 return false;
             }
